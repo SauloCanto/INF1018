72d3eb8ca03b9049409d412cfaefe48195a00538
hugo.freires <hugo.freires@ecoa.puc-rio.br>
2025-11-28 15:02:25 -0300

    Função zret e call implementadas e testadas com os arquivos novos criados. Todos os testes passaram.

--- gera_codigo.c
+++ gera_codigo.c
@@ -1,4 +1,5 @@
 /* Saulo Canto 2320940 3WB */
+/* Hugo Freires 2321223 3WA */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -82,123 +83,183 @@ void gera_codigo (FILE *f, unsigned char code[], funcp *entry){
         }
         else if (cmd == 'z') {  // zret
             printf("DEBUG: zret detectado\n");
-        }
-        else if (cmd == 'v') {  // atribuição (v0 = ...)
-            printf("DEBUG: atribuição detectada\n");
+            char var_cond[10];
+            char var_ret[10];
             
-            int var_dest;
-            char op1[10], op2[10];
-            char operador;
-            
-            if (sscanf(linha, "v%d = %s %c %s", &var_dest, op1, &operador, op2) == 4) {
-                printf("DEBUG: v%d = %s %c %s\n", var_dest, op1, operador, op2);
-                
-                if (strcmp(op1, "p0") == 0) {
-                    code[code_idx++] = 0x89;     // movl %edi, %ecx
-                    code[code_idx++] = 0xf9;
-                } else if (op1[0] == 'v') {
-                    int var_num = op1[1] - '0'; 
-                    int offset = 8 * (var_num + 1);
-                    unsigned char off_byte = 256 - offset;
-                    code[code_idx++] = 0x8b;    // movl -offset(%rbp), %ecx
-                    code[code_idx++] = 0x4d;
-                    code[code_idx++] = off_byte;
-                } else if (op1[0] == '$') {
-                    int const_val = atoi(&op1[1]);
-                    code[code_idx++] = 0xb9;    // movl $const, %ecx
-                    code[code_idx++] = (const_val & 0xFF);
-                    code[code_idx++] = ((const_val >> 8) & 0xFF);
-                    code[code_idx++] = ((const_val >> 16) & 0xFF);
-                    code[code_idx++] = ((const_val >> 24) & 0xFF);
-                }
-                
-                if (op2[0] == '$') {
-                    int const_val = atoi(&op2[1]);
-                    
-                    if (operador == '+') {
-                        // addl $const, %ecx
-                        if (const_val >= -128 && const_val <= 127) {
-                            code[code_idx++] = 0x83;
-                            code[code_idx++] = 0xc1;
-                            code[code_idx++] = (const_val & 0xFF);
-                        } else {
-                            code[code_idx++] = 0x81;
-                            code[code_idx++] = 0xc1;
-                            code[code_idx++] = (const_val & 0xFF);
-                            code[code_idx++] = ((const_val >> 8) & 0xFF);
-                            code[code_idx++] = ((const_val >> 16) & 0xFF);
-                            code[code_idx++] = ((const_val >> 24) & 0xFF);
-                        }
-                    } else if (operador == '-') {
-                        // subl $const, %ecx
-                        if (const_val >= -128 && const_val <= 127) {
-                            code[code_idx++] = 0x83;
-                            code[code_idx++] = 0xe9;
-                            code[code_idx++] = (const_val & 0xFF);
-                        } else {
-                            code[code_idx++] = 0x81;
-                            code[code_idx++] = 0xe9;
-                            code[code_idx++] = (const_val & 0xFF);
-                            code[code_idx++] = ((const_val >> 8) & 0xFF);
-                            code[code_idx++] = ((const_val >> 16) & 0xFF);
-                            code[code_idx++] = ((const_val >> 24) & 0xFF);
-                        }
-                    } else if (operador == '*') {
-                        // imull $const, %ecx, %ecx
-                        code[code_idx++] = 0x69;
-                        code[code_idx++] = 0xc9;
-                        code[code_idx++] = (const_val & 0xFF);
-                        code[code_idx++] = ((const_val >> 8) & 0xFF);
-                        code[code_idx++] = ((const_val >> 16) & 0xFF);
-                        code[code_idx++] = ((const_val >> 24) & 0xFF);
-                    }
-                } else {
-                    // Operação com registrador - carregar operando2 em %edx primeiro
-                    if (strcmp(op2, "p0") == 0) {
-                        // movl %edi, %edx
-                        code[code_idx++] = 0x89;
-                        code[code_idx++] = 0xfa;
-                    } else if (op2[0] == 'v') {
-                        // movl -offset(%rbp), %edx
-                        int var_num = op2[1] - '0';
-                        int offset = 8 * (var_num + 1);
-                        unsigned char off_byte = 256 - offset;
-                        code[code_idx++] = 0x8b;
-                        code[code_idx++] = 0x55;
-                        code[code_idx++] = off_byte;
-                    }
-                    
-                    // Aplicar operação
-                    if (operador == '+') {
-                        // addl %edx, %ecx
-                        code[code_idx++] = 0x01;
-                        code[code_idx++] = 0xd1;
-                    } else if (operador == '-') {
-                        // subl %edx, %ecx
-                        code[code_idx++] = 0x29;
-                        code[code_idx++] = 0xd1;
-                    } else if (operador == '*') {
-                        // imull %edx, %ecx
-                        code[code_idx++] = 0x0f;
-                        code[code_idx++] = 0xaf;
-                        code[code_idx++] = 0xca;
-                    }
-                }
-                
-                // 3. Salvar resultado em var_dest
-                int offset = 8 * (var_dest + 1);
-                unsigned char off_byte = 256 - offset;
-                // movl %ecx, -offset(%rbp)
-                code[code_idx++] = 0x89;
-                code[code_idx++] = 0x4d;
-                code[code_idx++] = off_byte;
-                
-                printf("DEBUG: Atribuição v%d gerada\n", var_dest);
-            }
+            // Lê: zret condicao valor_retorno
+            if (sscanf(linha, "zret %s %s", var_cond, var_ret) == 2) {
+                printf("DEBUG: zret %s %s\n", var_cond, var_ret);
 
+                // Carrega o valor da condição (var_cond) para %ecx
+                if (strcmp(var_cond, "p0") == 0) {
+                    code[code_idx++] = 0x8b; code[code_idx++] = 0xcf; // mov %edi, %ecx
+                } else if (var_cond[0] == '$') {
+                    int val = atoi(&var_cond[1]);
+                    code[code_idx++] = 0xb9; // mov $val, %ecx
+                    code[code_idx++] = val & 0xFF;
+                    code[code_idx++] = (val >> 8) & 0xFF;
+                    code[code_idx++] = (val >> 16) & 0xFF;
+                    code[code_idx++] = (val >> 24) & 0xFF;
+                } else { // é variável local vX
+                    int var_num = var_cond[1] - '0';
+                    int offset = 8 * (var_num + 1);
+                    code[code_idx++] = 0x8b; code[code_idx++] = 0x4d; // mov -off(%rbp), %ecx
+                    code[code_idx++] = (unsigned char)(256 - offset);
+                }
+
+                // cmpl $0, %ecx -> 83 f9 00
+                code[code_idx++] = 0x83;
+                code[code_idx++] = 0xf9;
+                code[code_idx++] = 0x00;
+
+                // Vamos pular o código de retorno. Precisamos saber quantos bytes pular.
+                // O código de retorno vai ter: MOVs (var_ret->eax) + LEAVE + RET.
+                // Deixar um placeholder para o offset do JNE e preencher depois.
+                code[code_idx++] = 0x75; // JNE (short jump)
+                int idx_do_pulo = code_idx; // Guarda onde escrever o tamanho do pulo
+                code[code_idx++] = 0x00; // Placeholder
+
+                // Move var_ret para %eax
+                if (strcmp(var_ret, "p0") == 0) {
+                    code[code_idx++] = 0x89; code[code_idx++] = 0xf8; // mov %edi, %eax
+                } else if (var_ret[0] == '$') {
+                    int val = atoi(&var_ret[1]);
+                    code[code_idx++] = 0xb8; // mov $val, %eax
+                    code[code_idx++] = val & 0xFF;
+                    code[code_idx++] = (val >> 8) & 0xFF;
+                    code[code_idx++] = (val >> 16) & 0xFF;
+                    code[code_idx++] = (val >> 24) & 0xFF;
+                } else { // vX
+                    int var_num = var_ret[1] - '0';
+                    int offset = 8 * (var_num + 1);
+                    code[code_idx++] = 0x8b; code[code_idx++] = 0x45; // mov -off(%rbp), %eax
+                    code[code_idx++] = (unsigned char)(256 - offset);
+                }
+
+                // Epílogo manual (leave + ret)
+                code[code_idx++] = 0xc9; // leave
+                code[code_idx++] = 0xc3; // ret
+
+                // O pulo deve ser: (indice atual) - (indice logo após a instrução JNE)
+                // indice logo após JNE = idx_do_pulo + 1
+                code[idx_do_pulo] = code_idx - (idx_do_pulo + 1);
+            }
         }
-        else {
-            printf("DEBUG: Comando desconhecido: %s\n", linha);
+        else if (cmd == 'v') { 
+            int var_dest;
+            char token1[20], token2[20], token3[20]; // Strings para ler os pedaços
+            
+            // Lê o formato genérico: v0 = token1 token2 token3
+            // Exemplo Soma: v0 = v1 + v2   -> token1="v1", token2="+", token3="v2"
+            // Exemplo Call: v0 = call 1 p0 -> token1="call", token2="1", token3="p0"
+            
+            if (sscanf(linha, "v%d = %s %s %s", &var_dest, token1, token2, token3) == 4) {
+                
+                // ==========================================================
+                // CASO 1: É UMA CHAMADA DE FUNÇÃO (CALL)
+                // ==========================================================
+                if (strcmp(token1, "call") == 0) {
+                    int id_func = atoi(token2); // Qual função chamar (0, 1, 2...)
+                    char *arg = token3;         // O argumento (p0, v0, $10...)
+
+                    printf("DEBUG: CALL detectado. Func: %d, Arg: %s\n", id_func, arg);
+
+                    if (arg[0] == '$') { // Constante
+                        int val = atoi(&arg[1]);
+                        code[code_idx++] = 0xbf; // mov $val, %edi
+                        code[code_idx++] = val & 0xFF;
+                        code[code_idx++] = (val >> 8) & 0xFF;
+                        code[code_idx++] = (val >> 16) & 0xFF;
+                        code[code_idx++] = (val >> 24) & 0xFF;
+                    } 
+                    else if (arg[0] == 'v') { // Variável Local
+                        int vn = arg[1] - '0';
+                        int offset = 8 * (vn + 1);
+                        // mov -offset(%rbp), %edi
+                        code[code_idx++] = 0x8b; 
+                        code[code_idx++] = 0x7d; 
+                        code[code_idx++] = (unsigned char)(256 - offset);
+                    }
+
+                    // 2. Gerar a instrução CALL
+                    // Opcode E8 (Call Relative)
+                    int end_instrucao_call = code_idx; 
+                    int target_addr = endereco_funcs[id_func]; // Endereço onde a função alvo começa
+                    
+                    // Offset = Destino - (Endereço_Onde_Estou + 5 bytes da instrução)
+                    int call_offset = target_addr - (end_instrucao_call + 5);
+
+                    code[code_idx++] = 0xe8;
+                    code[code_idx++] = call_offset & 0xFF;
+                    code[code_idx++] = (call_offset >> 8) & 0xFF;
+                    code[code_idx++] = (call_offset >> 16) & 0xFF;
+                    code[code_idx++] = (call_offset >> 24) & 0xFF;
+
+                    // 3. Pegar o retorno (%eax) e salvar na variável destino
+                    // mov %eax, -offset_destino(%rbp)
+                    int off_dest = 8 * (var_dest + 1);
+                    code[code_idx++] = 0x89; 
+                    code[code_idx++] = 0x45; 
+                    code[code_idx++] = (unsigned char)(256 - off_dest);
+                }
+                
+                // ==========================================================
+                // CASO 2: É UMA OPERAÇÃO ARITMÉTICA (CÓDIGO ANTIGO ADAPTADO)
+                // ==========================================================
+                else {
+                    // token1 é o operando 1 (ex: "v1")
+                    // token2 é o operador (ex: "+")
+                    // token3 é o operando 2 (ex: "v2")
+                    
+                    char *op1 = token1;
+                    char operador = token2[0];
+                    char *op2 = token3;
+                    
+                    // 1. Carregar op1 em %ecx
+                    if (strcmp(op1, "p0") == 0) {
+                        code[code_idx++] = 0x89; code[code_idx++] = 0xf9; // mov %edi, %ecx
+                    } else if (op1[0] == 'v') {
+                        int vn = op1[1] - '0';
+                        int off = 8 * (vn + 1);
+                        code[code_idx++] = 0x8b; code[code_idx++] = 0x4d; code[code_idx++] = 256-off;
+                    } else if (op1[0] == '$') {
+                        int val = atoi(&op1[1]);
+                        code[code_idx++] = 0xb9; 
+                        code[code_idx++] = val & 0xFF; code[code_idx++] = (val>>8)&0xFF; code[code_idx++] = (val>>16)&0xFF; code[code_idx++] = (val>>24)&0xFF;
+                    }
+
+                    // 2. Operar com op2
+                    // Precisamos carregar op2 ou usar imediato
+                    if (op2[0] == '$') {
+                        int val = atoi(&op2[1]);
+                        if (operador == '+') {
+                            if (val >= -128 && val <= 127) { code[code_idx++] = 0x83; code[code_idx++] = 0xc1; code[code_idx++] = val&0xFF; }
+                            else { code[code_idx++] = 0x81; code[code_idx++] = 0xc1; code[code_idx++] = val&0xFF; code[code_idx++] = (val>>8)&0xFF; code[code_idx++] = (val>>16)&0xFF; code[code_idx++] = (val>>24)&0xFF; }
+                        } else if (operador == '-') {
+                            if (val >= -128 && val <= 127) { code[code_idx++] = 0x83; code[code_idx++] = 0xe9; code[code_idx++] = val&0xFF; }
+                            else { code[code_idx++] = 0x81; code[code_idx++] = 0xe9; code[code_idx++] = val&0xFF; code[code_idx++] = (val>>8)&0xFF; code[code_idx++] = (val>>16)&0xFF; code[code_idx++] = (val>>24)&0xFF; }
+                        } else if (operador == '*') {
+                            code[code_idx++] = 0x69; code[code_idx++] = 0xc9; 
+                            code[code_idx++] = val&0xFF; code[code_idx++] = (val>>8)&0xFF; code[code_idx++] = (val>>16)&0xFF; code[code_idx++] = (val>>24)&0xFF;
+                        }
+                    } else {
+                        // op2 é variável ou p0. Carrega em %edx
+                        if (strcmp(op2, "p0") == 0) { code[code_idx++] = 0x89; code[code_idx++] = 0xfa; }
+                        else if (op2[0] == 'v') {
+                            int vn = op2[1] - '0'; int off = 8*(vn+1);
+                            code[code_idx++] = 0x8b; code[code_idx++] = 0x55; code[code_idx++] = 256-off;
+                        }
+                        
+                        if (operador == '+') { code[code_idx++] = 0x01; code[code_idx++] = 0xd1; } // add %edx, %ecx
+                        else if (operador == '-') { code[code_idx++] = 0x29; code[code_idx++] = 0xd1; } // sub %edx, %ecx
+                        else if (operador == '*') { code[code_idx++] = 0x0f; code[code_idx++] = 0xaf; code[code_idx++] = 0xca; } // imul %edx, %ecx
+                    }
+
+                    // 3. Salvar resultado (que está em %ecx) na variavel destino
+                    int off_dest = 8 * (var_dest + 1);
+                    code[code_idx++] = 0x89; code[code_idx++] = 0x4d; code[code_idx++] = 256-off_dest;
+                }
+            }
         }
     }
     
--- test_all.sh
+++ test_all.sh
@@ -48,3 +48,45 @@ echo "  Com argumento 5 (esperado: 25):"
 ./teste test_f6.txt 5 | grep "Resultado:"
 echo "  Com argumento 10 (esperado: 40):"
 ./teste test_f6.txt 10 | grep "Resultado:"
+
+# Testando o zret
+echo ""
+echo "=== Testando zret ==="
+echo "ARQUIVO 1: zret p0 \$5 (se 0 retorna 5, senão segue e retorna p0)"
+
+echo "  Com argumento 0 (esperado: 5 - entrou no zret):"
+./teste test_zret_1.txt 0 | grep "Resultado:"
+
+echo "  Com argumento 5 (esperado: 5 - pulou zret, retornou p0):"
+./teste test_zret_1.txt 5 | grep "Resultado:"
+
+echo ""
+echo "ARQUIVO 2: zret p0 p0 (se 0 retorna 0, senão segue e retorna 99)"
+
+echo "  Com argumento 0 (esperado: 0 - entrou no zret):"
+./teste test_zret_2.txt 0 | grep "Resultado:"
+
+echo "  Com argumento 5 (esperado: 99 - pulou zret, retornou 99):"
+./teste test_zret_2.txt 5 | grep "Resultado:"
+
+echo ""
+echo "=== Testando CALL (Chamada de Função) ==="
+
+echo "Teste 1: Chamada Simples (Encadeada)"
+echo "Arquivo: test_call_1.txt"
+echo "Lógica: Chama a função 'inc' duas vezes. f(x) = x + 2"
+echo "  Com argumento 10 (esperado: 12):"
+./teste test_call_1.txt 10 | grep "Resultado:"
+echo "  Com argumento 0 (esperado: 2):"
+./teste test_call_1.txt 0 | grep "Resultado:"
+
+echo ""
+echo "Teste 2: Recursão e Múltiplas Funções (Soma dos Quadrados)"
+echo "Arquivo: test_call_recursivo.txt"
+echo "Lógica: Soma dos quadrados de 1 até N"
+echo "  Com argumento 3 (esperado: 14 -> 9+4+1):"
+./teste test_call_recursivo.txt 3 | grep "Resultado:"
+echo "  Com argumento 4 (esperado: 30 -> 16+9+4+1):"
+./teste test_call_recursivo.txt 4 | grep "Resultado:"
+echo "  Com argumento 5 (esperado: 55):"
+./teste test_call_recursivo.txt 5 | grep "Resultado:"
